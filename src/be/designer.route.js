efa8da4437ef2fb2e405c97e38402bb0b9c49fef
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2024-03-12 23:33:59 +0700

    designer route

---
+++ src/be/designer.route.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const Designer = require('../models/designer.model');
+
+// Get all designers
+router.get('/', async (req, res) => {
+    try {
+        const designers = await Designer.find();
+        res.json(designers);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Create a new designer
+router.post('/', async (req, res) => {
+    const designer = new Designer({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const newDesigner = await designer.save();
+        res.status(201).json(newDesigner);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Update designer
+router.patch('/:id', async (req, res) => {
+    try {
+        const designer = await Designer.findById(req.params.id);
+        if (req.body.name) {
+            designer.name = req.body.name;
+        }
+        if (req.body.email) {
+            designer.email = req.body.email;
+        }
+        if (req.body.password) {
+            designer.password = req.body.password;
+        }
+        const updatedDesigner = await designer.save();
+        res.json(updatedDesigner);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Delete designer
+router.delete('/:id', async (req, res) => {
+    try {
+        const designer = await Designer.findById(req.params.id);
+        await designer.remove();
+        res.json({ message: 'Designer deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
