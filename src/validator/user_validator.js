b938383e67d6ba9ad3efbc6a780d6ec4359577fd
daoduc220901 <90316727+concuacon1@users.noreply.github.com>
2024-03-18 17:01:17 +0700

    validation

--- src/validator/user_validator.js
+++ src/validator/user_validator.js
@@ -1,4 +1,4 @@
-const {enum_role} = require("../common/enum.database")
+const { enum_role } = require("../common/enum.database")
 const minFullName = 5
 const maxFullName = 100
 
@@ -47,24 +47,71 @@ const createUserValidatorSchema = {
     role: {
         notEmpty: {
             errorMessage: "Quyền không được để trống"
-        }, 
+        },
         isIn: {
             options: [enum_role],
             errorMessage: "Không đúng định dạng quyền"
         }
     },
-    phoneNumber : {
+    phoneNumber: {
         notEmpty: {
             errorMessage: "phoneNumber không được để trống"
-        }, 
+        },
         matches: {
-            options: /^(03|05|07|08|09)+([0-9]{8})$/, 
+            options: /^(03|05|07|08|09)+([0-9]{8})$/,
             errorMessage: "phoneNumber không hợp định dạng"
         }
     }
 }
 
 
+const validatorPasswordChange = {
+    passwordOld: {
+        notEmpty: {
+            errorMessage: "Mật khẩu cũ không được để trống"
+        },
+        isString: {
+            errorMessage: "Mật khẩu cũ phải là chuỗi string"
+        }
+    },
+    passwordNew: {
+        notEmpty: {
+            errorMessage: "Mật khẩu mới không được để trống"
+        },
+        isLength: {
+            options: {
+                min: minFullName,
+                max: maxFullName
+            },
+            errorMessage: `Mật khẩu mới  có tối thiểu ${minFullName} kí tự và tối đa ${maxFullName} kí tự`
+        },
+        isString: {
+            errorMessage: "Mật khẩu mới  phải là chuỗi string"
+        }
+    },
+}
+
+
+const validatorPasswordChangeOTP = {
+    passwordNew: {
+        notEmpty: {
+            errorMessage: "Mật khẩu mới không được để trống"
+        },
+        isLength: {
+            options: {
+                min: minFullName,
+                max: maxFullName
+            },
+            errorMessage: `Mật khẩu mới  có tối thiểu ${minFullName} kí tự và tối đa ${maxFullName} kí tự`
+        },
+        isString: {
+            errorMessage: "Mật khẩu mới  phải là chuỗi string"
+        }
+    },
+}
+
 module.exports = {
-    createUserValidatorSchema
+    createUserValidatorSchema,
+    validatorPasswordChange,
+    validatorPasswordChangeOTP
 }
