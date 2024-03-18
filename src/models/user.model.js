be281c3c26bf09f4965184b829a16b8bda119f34
daoduc220901 <90316727+concuacon1@users.noreply.github.com>
2024-03-18 16:59:46 +0700

    update user models, add category, projectType

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,80 +1,58 @@
-const mongoose = require("mongoose");
+
+
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const { enum_role } = require("../common/enum.database")
+const {enum_role} = require("../common/enum.database")
 
-const USER_MODEL = "users";
-
-const userSchema = new Schema(
-  {
-    firstName: {
-      type: String,
-      maxlength: 32,
-      minlength: 1,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      maxlength: 32,
-      minlength: 1,
-      required: true,
-    },
+const UserSchema = new Schema({
     fullName: {
-      type: String,
-      required: false,
-      default: null,
-    },
-    userCode: {
-      type: String,
-      default: ""
-    },
-    address: {
-      type: String
+        type: String,
     },
     email: {
-      type: String,
-      maxlength: 64,
-      minlength: 1,
-      required: true,
-    },
-    phoneNumber: {
-      type: String
-    },
-    dob: {
-      type: Date,
-      default: ""
-    },
-    gender: {
-      type: String,
-      enum: ["MALE", "FEMALE"],
-      required: true,
+        type: String,
     },
     password: {
-      type: String,
-      required: true,
+        type: String
+    },
+    userCode: {
+        type: String,
+        default: ""
+    },
+    address: {
+        type: String,
+        default: ""
+    },
+    dob: {
+        type: Date,
+        default: ""
+    },
+    gender: {
+        type: String
     },
     code_change_password: {
-      type: String,
-      default: ""
+        type: String,
+        default: ""
+    },
+    phoneNumber: {
+        type: String
     },
     description: {
-      type: String,
-      default: ""
+        type: String,
+        default: ""
     },
     role: {
-      type: String,
-      enum: enum_role,
-      required: true,
-    },
+        type: String,
+        enum: enum_role,
+      },
     isActive: {
-      type: Boolean,
-      default: true
+        type: Boolean,
+        default: true
     },
-    isDelete: {
-      type: Boolean,
-      default: false
+    isDelete : {
+        type: Boolean,
+        default: false
     },
-  },
-  { timestamps: true, collection: USER_MODEL },
-);
 
-module.exports = mongoose.model(USER_MODEL, userSchema);
+}, { timestamps: true })
+
+module.exports = mongoose.model("users", UserSchema)
