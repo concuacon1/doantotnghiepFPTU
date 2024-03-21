9eab7bcde4a27a3c8bf055a7f782edad10abaca8
daoduc220901 <90316727+concuacon1@users.noreply.github.com>
2024-03-21 14:42:21 +0700

    schedule controller

---
+++ src/models/schedule.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const { enum_status_schedule  , time_select_schedule} = require("../common/enum.database")
+
+const ScheduleSchema = new Schema({
+    designerId:  { type: Schema.Types.ObjectId , ref: 'designers' },
+    customerId:  { type: Schema.Types.ObjectId , ref: 'customer' ,default : null},
+    description_off: {
+        type: String,
+        default : ""
+    },
+    description_book: {
+        type: String,
+        default : ""
+    },
+    status : {
+        type: String,
+        enum: enum_status_schedule,
+        default: "NULL"
+    },
+    isSelectBook : {
+        type: Boolean,
+        default: false
+    },
+    workOn : {
+        type: Boolean,
+        default: false
+    },
+    timeSelect : {
+        type: String,
+        enum : time_select_schedule,
+        default: "NULL"
+    },
+    timeWork : {
+        type: Date,
+        default: ""
+    }
+
+
+}, { timestamps: true })
+
+module.exports = mongoose.model("schedule", ScheduleSchema)
